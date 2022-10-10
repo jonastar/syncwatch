1a952c75f134c7f03a85eefff9a4d7f03a3c2874
jonastar <98033487+jonastar@users.noreply.github.com>
2022-10-10 23:10:01 +0200

    proper ffmpeg command

--- encode_sample.sh
+++ encode_sample.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # This gives decent quality, and burns in subtitles
-ffmpeg -i "$1" -vf subtitles="$1" -c:v h264 -preset slow -crf 22 out.mp4
+ffmpeg -i "$1" -vf "subtitles='$1'" -c:v h264 -preset slow -crf 22 out.mp4
